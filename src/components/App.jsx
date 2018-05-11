070d6ac7e18eadfaa6980f0704284349420032cc
Matheus Faria de Alencar <mtsalenc@gmail.com>
2018-05-10 21:39:41 -0300

    feat: Create static root list layout

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,50 +1,67 @@
-import React, { Component } from 'react';
-import Jumbotron from 'react-bootstrap/lib/Jumbotron';
-import Button from 'react-bootstrap/lib/Button';
-import Navbar from 'react-bootstrap/lib/Navbar';
-import Nav from 'react-bootstrap/lib/Nav';
-import NavItem from 'react-bootstrap/lib/NavItem';
-import NavDropdown from 'react-bootstrap/lib/NavDropdown';
-import MenuItem from 'react-bootstrap/lib/MenuItem';
-
+import React, { Component } from 'react'
+import Navbar from './Navbar'
+import Jumbotron from 'react-bootstrap/lib/Jumbotron'
+import Button from 'react-bootstrap/lib/Button'
+import ListGroup from 'react-bootstrap/lib/ListGroup'
+import ListGroupItem from 'react-bootstrap/lib/ListGroupItem'
+import Media from 'react-bootstrap/lib/Media'
+import Label from 'react-bootstrap/lib/Label'
+import ButtonToolbar from 'react-bootstrap/lib/ButtonToolbar'
+import DropdownButton from 'react-bootstrap/lib/DropdownButton'
+import MenuItem from 'react-bootstrap/lib/MenuItem'
+import SplitButton from 'react-bootstrap/lib/SplitButton'
+import Well from 'react-bootstrap/lib/Well'
 
 class App extends Component {
   render() {
     return (
       <div>
-        <Navbar inverse collapseOnSelect>
-          <Navbar.Header>
-            <Navbar.Brand>
-              <a href="#brand">Readable</a>
-            </Navbar.Brand>
-            <Navbar.Toggle />
-          </Navbar.Header>
-          <Navbar.Collapse>
-            <Nav>
-              <NavItem eventKey={1} href="#">
-                Link
-              </NavItem>
-              <NavItem eventKey={2} href="#">
-                Link
-              </NavItem>
-            </Nav>
-          </Navbar.Collapse>
-        </Navbar>
+        <Navbar />
         <div className="container">
-          <Jumbotron>
-            <h1>Hello, react!</h1>
-            <p>
-              This is a simple hero unit, a simple jumbotron-style component for calling
-              extra attention to featured content or information.
-            </p>
-            <p>
-              <Button bsStyle="primary">Learn more</Button>
-            </p>
-          </Jumbotron>
+          <h2>Category</h2>
+          <br/>
+          <ListGroup>
+            <ListGroupItem>
+              <div>
+                <Media>
+                  <Media.Body>
+                    <Media.Heading>
+                      <strong>Title</strong>
+                    </Media.Heading>
+                    <p>
+                      Author
+                      <small> - 34 comments - </small>
+                      <small><Label bsStyle="primary">42</Label></small>
+                    </p>
+                  </Media.Body>
+                  <Media.Right>
+                    <Button
+                      className="glyphicon glyphicon-pencil"
+                      bsSize="small" />
+                  </Media.Right>
+                  <Media.Right>
+                    <Button
+                      className="glyphicon glyphicon-trash"
+                      bsSize="small" />
+                  </Media.Right>
+                  <Media.Right>
+                    <Button
+                      className="glyphicon glyphicon-chevron-up"
+                      bsSize="small" />
+                    <Button
+                      className="glyphicon glyphicon-chevron-down"
+                      bsSize="small" />
+                  </Media.Right>
+                </Media>
+              </div>
+            </ListGroupItem>
+            <ListGroupItem>Item 2</ListGroupItem>
+            <ListGroupItem>...</ListGroupItem>
+          </ListGroup>
         </div>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
